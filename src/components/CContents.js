cf28ced9e188a129e31fab3c6a6bb75bae3c5737
Krystian Dutka <krystian.dutka@gmail.com>
2021-01-22 20:51:11 +0100

    React-01-wste[

---
+++ src/components/CContents.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react'
+
+class CContents extends Component {
+
+    constructor(){
+        super()
+        this.state = {msg: 'Witaj użytkowniku. Kliknij przycisk aby poznać wiadomość'}
+    }
+
+    changeMsg(){
+        this.setState({msg: 'Właśnie jesteś uczestnikiem kursu React,gratulacje '});
+    }
+
+render(){
+    return (
+        <div>
+            <h1>{this.state.msg}</h1>
+            <button onClick={()=>this.changeMsg()}>Kliknij,aby poznać tajemnicę</button>
+        </div>
+        )
+    }   
+
+}
+
+export default CContents
